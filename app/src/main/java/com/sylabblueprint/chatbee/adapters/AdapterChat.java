cb29d5025e640868c5815befa23b1406c784bc94
Abraham Sylvester <mrabblessedslyst4christ099@gmail.com>
2019-11-06 21:55:47 +0200

    Messaging feature update

---
+++ app/src/main/java/com/sylabblueprint/chatbee/adapters/AdapterChat.java
@@ -0,0 +1,135 @@
+package com.sylabblueprint.chatbee.adapters;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+import com.sylabblueprint.chatbee.R;
+import com.sylabblueprint.chatbee.models.ModelChat;
+
+public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
+
+    private static final int MSG_TYPE_LEFT = 0;
+    private static final int MSG_TYPE_RIGHT = 1;
+    Context context;
+    List<ModelChat> chatList;
+    String imageUrl;
+
+    FirebaseUser fUser;
+
+
+    public AdapterChat(Context context, List<ModelChat> chatList, String imageUrl) {
+        this.context = context;
+        this.chatList = chatList;
+        this.imageUrl = imageUrl;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        //inflate layouts: row_chat_left.xml for receiver, row_chat_right.xml for sender
+        if (i==MSG_TYPE_RIGHT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_right, parent, false);
+            return new MyHolder(view);
+        } else
+        {
+
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_left, parent, false);
+            return new MyHolder(view);
+
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int i) {
+        //get data
+        String message = chatList.get(i).getMessage();
+        String timeStamp = chatList.get(i).getTimestamp();
+
+        //Convert time stamp to dd/mm/yyy hh:mm am/pm
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(Long.parseLong(timeStamp));
+        String dateTime = DateFormat.format("dd/MM/yyyy, hh:mm aa", cal).toString();
+
+
+        //Set data
+        holder.messageTv.setText(message);
+        holder.timeTv.setText(dateTime);
+        try {
+            Picasso.get().load(imageUrl).into(holder.profileIv);
+        }
+        catch (Exception e) {
+
+        }
+
+        //set seen/delivered status of message
+        if (i == chatList.size() -1){
+            if (chatList.get(i).isSeen()) {
+                holder.isSeenTv.setText("Seen");
+            }
+            else  {
+                holder.isSeenTv.setText("Delivered");
+
+            }
+        }
+
+        else {
+            holder.isSeenTv.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //Get current signed in user
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (chatList.get(position).getSender().equals(fUser.getUid())) {
+            return MSG_TYPE_RIGHT;
+        } else  {
+            return MSG_TYPE_LEFT;
+        }
+    }
+
+    //View holder class
+    class MyHolder extends RecyclerView.ViewHolder {
+
+        //views
+        ImageView profileIv;
+        TextView messageTv, timeTv, isSeenTv;
+
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //Init views
+            profileIv = itemView.findViewById(R.id.profileIv);
+            messageTv = itemView.findViewById(R.id.messageTv);
+            timeTv = itemView.findViewById(R.id.timeTv);
+            isSeenTv = itemView.findViewById(R.id.isSeenTv);
+
+
+        }
+    }
+
+}
--- app/src/main/java/com/sylabblueprint/chatbee/adapters/ChatActivity.java
+++ app/src/main/java/com/sylabblueprint/chatbee/adapters/ChatActivity.java
@@ -1,17 +1,40 @@
 package com.sylabblueprint.chatbee.adapters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.sylabblueprint.chatbee.MainActivity;
 import com.sylabblueprint.chatbee.R;
+import com.sylabblueprint.chatbee.models.ModelChat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
 
     //Views from XML
@@ -21,6 +44,26 @@ public class ChatActivity extends AppCompatActivity {
     TextView nameTv, userStatusTv;
     EditText messageEt;
     ImageButton sendBtn;
+
+
+    //Firebase Auth
+    FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference  usersDbRef;
+
+    //For checking if user has seen the message or not
+    ValueEventListener seenListener;
+    DatabaseReference userRefForSeen;
+
+    List<ModelChat> chatList;
+     AdapterChat adapterChat;
+
+    String hisUid;
+    String myUid;
+    String hisImage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +81,211 @@ public class ChatActivity extends AppCompatActivity {
         messageEt = findViewById(R.id.messageEt);
         sendBtn = findViewById(R.id.sendBtn);
 
+        //Layout (LinearLayout) For  RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+
+        //recyclerview properties
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        //On clicking user from the user list, we have passed the user ID using intent
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("hisUid");
+
+
+        //Firebase Auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usersDbRef  = firebaseDatabase.getReference("Users");
+
+        //search user to get user's info
+        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
+
+        //get user  picture and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Check until required info is received
+                for (DataSnapshot ds:  dataSnapshot.getChildren()) {
+                    //Get all data
+                    String name =" " + ds.child("name").getValue();
+                    hisImage =" " + ds.child("image").getValue();
+
+                    //Set data
+                    nameTv.setText(name);
+                    try {
+                        //image received, set it to imageView in toolbar
+                        Picasso.get().load(hisImage).placeholder(R.drawable.ic_default_img_white).into(profileIv);
+
+                    }
+                    catch (Exception e) {
+
+                        //There is an exception getting picture, set default  picture
+                        Picasso.get().load(R.drawable.ic_default_img_white).into(profileIv);
+
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //Click button to send message
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get text from editText field
+                String message = messageEt.getText().toString().trim();
+                //Check if text field is empty or not
+                if (TextUtils.isEmpty(message)) {
+                    //Text field is empty
+                    Toast.makeText(ChatActivity.this, "Cannot send an empty message...", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //Text field not empty
+                    sendMessage(message);
+                }
+            }
+        });
+
+            readMessage();
+
+            seenMessage();
+
+    }
+
+    private void seenMessage() {
+
+        userRefForSeen = FirebaseDatabase.getInstance().getReference("Chats");
+        seenListener = userRefForSeen.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelChat chat  = ds.getValue(ModelChat.class);
+                    if (chat.getReceiver().equals(myUid) && chat.getSender().equals(hisUid)) {
+                        HashMap<String, Object> hasSeenHashMap = new HashMap<>();
+                        hasSeenHashMap.put("isSeen", true);
+                        ds.getRef().updateChildren(hasSeenHashMap);
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void readMessage(){
+         chatList = new ArrayList<>();
+         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
+         dbRef.addValueEventListener(new ValueEventListener() {
+             @Override
+             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    chatList.clear();
+                    for (DataSnapshot ds: dataSnapshot.getChildren()){
+                        ModelChat chat = ds.getValue(ModelChat.class);
+                        if (chat.getReceiver().equals(myUid) &&  chat.getSender().equals(hisUid) ||
+                                chat.getReceiver().equals(hisUid) &&  chat.getSender().equals(myUid)) {
+                            chatList.add(chat);
+                        }
+                        //Adapter
+                        adapterChat = new AdapterChat(ChatActivity.this, chatList, hisImage);
+                        adapterChat.notifyDataSetChanged();
+                        //set adapter to recyclerview
+                        recyclerView.setAdapter(adapterChat);
+                    }
+             }
+
+             @Override
+             public void onCancelled(@NonNull DatabaseError databaseError) {
+
+             }
+         });
+
+    }
+
+    private void sendMessage(String message) {
+        //Chat node will be created  that will contain all chats history
+
+        DatabaseReference databaseReference = FirebaseDatabase .getInstance().getReference();
+
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+        HashMap<String, Object> hashMap  = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message", message);
+        hashMap.put("timestamp", timestamp);
+        hashMap.put("isSeen", false);
+
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //Reset edit text after sending message
+        messageEt.setText("");
+    }
+
+    private void checkUserStatus(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null){
+            //user is signed in stay here
+            //Set email of Logged in User
+
+            //mProfileTv.setText(user.getEmail());
+            myUid = user.getUid(); // Currently signed in user's uid
+
+
+
+        }else {
+            //User not signed in, go to main activity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        userRefForSeen.removeEventListener(seenListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        //hide searchView, as we don't need it here!
+        menu.findItem(R.id.action_search).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //Get item id
+        int id = item.getItemId();
+        if(id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
